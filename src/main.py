9279999fea2bf11f3904cc30bf480b23840e0e3f
Pablo Jiménez García <pablojimenezgarcia1@gmail.com>
2019-11-07 23:18:14 +0100

    Addicción de input y gestión de acciones en el main

--- src/main.py
+++ src/main.py
@@ -10,22 +10,33 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     # logging(str(sys.thread_info))
 
+    #todo DISEÑAR SISTEMA PARA MODULAR EL MAIN MEJOR EN ACCIONES
+
+    accion = str(input("Quieres escribir o leer?"))
     cantante = sys.argv[1]
-    lista_url_de_canciones = Scrapper.get_urls_from_songs_file(sys.argv[1] + "_canciones.txt")
     estilo = sys.argv[2]
     pais = sys.argv[3]
 
-    logging.info("cantante: " + sys.argv[1])
-    logging.info("estilo: " + cantante)
-    logging.info("pais: " + cantante)
+    def loggeo_principio_proceso(accion, cantante, estilo, pais):
+        logging.info("accion: " + accion)
+        logging.info("cantante: " + cantante)
+        logging.info("estilo: " + estilo)
+        logging.info("pais: " + pais)
 
-    # 2. POR CADA CANCION GENERAMOS UNA LÍNEA EN EL FICHERO DE SALIDA
-    for cancion in lista_url_de_canciones:
-        counter_por_cancion = Scrapper.get_text_from_letras_com_soup(cantante, cancion)
 
-    # 3. ESCRIBIMOS LOS RESULTADOS EN MONGO, DENTRO DE LA COLECCIÓN artists_raw
-    MongoWriter.file_to_mongo(cantante + ".txt", cantante, estilo, pais)
+    if accion.lower() == "escribir":
+        loggeo_principio_proceso(accion, cantante, estilo, pais)
+        lista_url_de_canciones = Scrapper.get_urls_from_songs_file(sys.argv[1] + "_canciones.txt")
 
-    stats = MongoReader.read_from_mongo(cantante)
+        for cancion in lista_url_de_canciones:
+            counter_por_cancion = Scrapper.get_text_from_letras_com_soup(cantante, cancion)
 
-    WordCloud.show_wordcloud(stats)
+        MongoWriter.file_to_mongo(cantante + ".txt", cantante, estilo, pais)
+
+    elif accion.lower() == "leer":
+        loggeo_principio_proceso(accion, cantante, estilo, pais)
+        stats = MongoReader.read_from_mongo(cantante, "text_raw")
+        WordCloud.show_wordcloud(stats)
+
+    else:
+        logging.error("Acción no comtemplada")
